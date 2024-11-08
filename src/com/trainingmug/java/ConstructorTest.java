b9dffb81bb2e0bf5eef4f8d59bff197325c05708
rajagopalreddy-tm <rajagopal.reddy@trainingmug.com>
2024-11-08 13:41:47 +0530

    no-arg-constructor program

---
+++ src/com/trainingmug/java/ConstructorTest.java
@@ -0,0 +1,13 @@
+package com.trainingmug.java;
+
+public class ConstructorTest {
+    public static void main(String[] args) {
+        Employee employee1 = new Employee();
+        employee1.employeeProfile();
+        //employee1.doSomeConfiguration();
+
+        Employee employee2 =new Employee();
+        employee2.employeeProfile();
+        //employee2.doSomeConfiguration();
+    }
+}
